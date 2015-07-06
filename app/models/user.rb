15961e3f1fb2d1cbb362204811cfaeaac604e60d
rhyseda <rhys_eda@portegys.org>
2015-07-07 11:26:15 +1200

    Added unique test from profile, added coded to pass test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ActiveRecord::Base
          validates :first_name, presence: true
 
          validates :last_name, presence: true
-         validates :profile_name, presence: true
+
+         validates :profile_name, presence: true,
+                                  uniqueness: true
 
   has_many :statuses
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,11 @@ class UserTest < ActiveSupport::TestCase
     assert !user.errors[:profile_name].empty?
   end
 
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:kermit).profile_name
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
 end
